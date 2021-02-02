742c3f7efbe78b685c582c6ce0e4aca640628c1f
Brendan-Fisher <btfisher1229@gmail.com>
2021-02-02 14:19:46 -0500

    added register/login functionality with connection to database

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { registerMember } from "../../redux/actions/authActions";
+import classnames from "classnames";
 
 class Register extends Component {
     constructor() {
@@ -11,6 +15,14 @@ class Register extends Component {
         password2: "",
         errors: {}
         };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors
+            });
+        }
     }
 
     onChange = e => {
@@ -25,7 +37,7 @@ class Register extends Component {
             password: this.state.password,
             password2: this.state.password2
         };
-        console.log(newMember);
+        this.props.registerMember(newMember, this.props.history);
     };
 
     render() {
@@ -52,8 +64,10 @@ class Register extends Component {
                                     error={errors.name}
                                     id="name"
                                     type="text"
+                                    className={classnames("",{ invalid: errors.name })}
                                 />
                                 <label htmlFor="name">Name</label>
+                                <span className="red-text">{errors.name}</span>
                             </div>
                             <div className="input-field col s12">
                                 <input
@@ -62,8 +76,10 @@ class Register extends Component {
                                 error={errors.email}
                                 id="email"
                                 type="email"
+                                className={classnames("",{ invalid: errors.email })}
                                 />
                                 <label htmlFor="email">Email</label>
+                                <span className="red-text">{errors.email}</span>
                             </div>
                             <div className="input-field col s12">
                                 <input
@@ -72,8 +88,10 @@ class Register extends Component {
                                 error={errors.password}
                                 id="password"
                                 type="password"
+                                className={classnames("",{ invalid: errors.password })}
                                 />
                                 <label htmlFor="password">Password</label>
+                                <span className="red-text">{errors.password}</span>
                             </div>
                             <div className="input-field col s12">
                                 <input
@@ -82,8 +100,10 @@ class Register extends Component {
                                 error={errors.password2}
                                 id="password2"
                                 type="password"
+                                className={classnames("",{ invalid: errors.password2 })}
                                 />
                                 <label htmlFor="password2">Confirm Password</label>
+                                <span className="red-text">{errors.password2}</span>
                             </div>
                             <div className="col s12" style={{ paddingLeft: "11.250px" }}>
                                 <button
@@ -107,4 +127,18 @@ class Register extends Component {
     }
 }
 
-export default Register;
+Register.propTypes = {
+    registerMember: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+});
+
+export default connect(
+    mapStateToProps,
+    { registerMember }
+)(withRouter(Register));
