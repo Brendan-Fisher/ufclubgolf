a2e79c7637982599a100c6888d0f57f570632872
Brendan-Fisher <btfisher1229@gmail.com>
2021-03-22 18:25:39 -0400

    added randomized golf quotes

--- frontend/src/components/features/ImageSlider.js
+++ frontend/src/components/features/ImageSlider.js
@@ -4,6 +4,62 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 import bg1 from '../../img/impressionImage.jpg';
 import bg2 from '../../img/bg.jpg';
 
+const quotes = [
+    {
+        quote: "Success in this game depends less on strength of body than strength of mind and character", 
+        author: "Arnold Palmer"
+    },
+    {
+        quote: "I get to play golf for a living. What more can you ask for, getting paid for doing what you love",
+        author: "Tiger Woods"
+    },
+    {
+        quote: "Stay true to yourself and listen to your inner voice. It will lead you to your dream.",
+        author: "James Ross"
+    },
+    {
+        quote: "A good golfer has the determination to win and the patience to wait for the breaks",
+        author: "Gary Player"
+    },
+
+    {
+        quote: "Golf is about how well you accept, respond to, and score with your misses much more so than it is a game of your perfect shots.",
+        author: "Dr. Bob Rotella"
+    },
+    {
+        quote: "I have to believe in myself. I know what I can do, what I can achieve.",
+        author: "Sergio Garcia"
+    },
+    {
+        quote: "Golf is a compromise between what your ego wants you to do, what experience tells you to do, and what your nerves let you do",
+        author: "Bruce Crampton"
+    },
+    {
+        quote: "As you walk down the fairway of life you must smell the roses, for you only get to play one round.",
+        author: "Ben Hogan"
+    },
+    {
+        quote: "Golf is a science, the study of a lifetime, in which you can exhaust yourself but never your subject",
+        author: "David Forgan"
+    },
+    {
+        quote: "A routine is not a routine if you have to think about it",
+        author: "Davis Love Jr."
+    },
+    {
+        quote: "I think it’s more than whether or not you win or lose. It’s having that opportunity on that final round, final nine, to come down the stretch with a chance to win",
+        author: "Phil Mickelson"
+    },
+    {
+        quote: "Always make a total effort, even when the odds are against you.",
+        author: "Arnold Palmer"
+    },
+    {
+        quote: "It’s about hitting the ball in the center of the club face and hitting it hard",
+        author: "Bubba Watson"
+    }
+]
+
 class ImageSlider extends Component
 {   
     componentDidMount()
@@ -20,12 +76,14 @@ class ImageSlider extends Component
         
         for(const [index, value] of srcImages.entries())
         {
+            var quote = quotes[Math.floor(Math.random() * quotes.length)]
+            
             items.push(
                 <li key={index}>
                     <img alt={"image number " + index + " has not found."} src={value} className="SliderImg"/>  
                     <div className="caption right-align">
                         <h3>Welcome to UF Golf Club!</h3>
-                        <h5 className="light grey-text text-lighten-3">There are no shortcuts on the quest for perfection <br/>- Ben Hogan</h5>
+                        <h5 className="light grey-text text-lighten-3">{quote.quote}<br/>- {quote.author}</h5>
                     </div>
                 </li>
             )
