c988a5f9d205c7874906ed9dccc77957b805a4f0
Brendan-Fisher <btfisher1229@gmail.com>
2021-04-12 00:44:38 -0400

    deploy attempt 6

--- backend/server.js
+++ backend/server.js
@@ -52,10 +52,10 @@ app.use("/api/events", events);
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
   // Set static folder
-  app.use(express.static('./frontent/build'))
+  app.use(express.static('../frontent/build'))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve('../frontend', 'build', 'index.html'));
   });
 }
 
