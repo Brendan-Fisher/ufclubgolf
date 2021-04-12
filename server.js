aca4a9d9d54953bfb7d6fd0da121e67ea2f82b81
Brendan-Fisher <btfisher1229@gmail.com>
2021-04-12 01:45:45 -0400

    deploy attempt 10, big typo guy

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use("/api/events", events);
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
   // Set static folder
-  app.use(express.static('frontent/build'))
+  app.use(express.static('frontend/build'))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
