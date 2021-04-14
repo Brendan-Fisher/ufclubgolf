9b64c55ea81f713c288f8dfbd784ab2c917e1b25
Brendan-Fisher <btfisher1229@gmail.com>
2021-04-13 22:37:43 -0400

    switched root admin to club email

--- validation/delete.js
+++ validation/delete.js
@@ -3,7 +3,7 @@ const isEmpty = require("is-empty");
 module.exports = function validateDeleteUser(data) {
     let errors = {};
 
-    if(data.email === "gatorsclubgolf@gmail.com"){
+    if(data.email === "floridaclubgolfteam@gmail.com"){
         errors.error = "Unable to delete root admin";
     }
 
