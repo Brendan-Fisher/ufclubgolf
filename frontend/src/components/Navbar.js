febdf37786a456e65b6d852669bb1b067711f2fe
Brendan-Fisher <btfisher1229@gmail.com>
2021-08-19 16:04:23 -0400

    Changed text styling on sidenav to be consistent

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -77,22 +77,22 @@ class Navbar extends Component {
                 </div>
                 <div>
                     <ul id="slide-out" className="sidenav sidenav-trigger">
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/calendar">Calendar</a></li>
-                        <li><a href="/tournaments">Tournaments</a></li>
-                        <li><a href="/posts">Posts</a></li>
+                        <li><a href="/" className="nav-item">Home</a></li>
+                        <li><a href="/about" className="nav-item">About</a></li>
+                        <li><a href="/calendar" className="nav-item">Calendar</a></li>
+                        <li><a href="/tournaments" className="nav-item">Tournaments</a></li>
+                        <li><a href="/posts" className="nav-item">Posts</a></li>
                         <Divider />
                         {loggedIn && memberType !== "pending" && memberType !== "member" &&
-                            <li><a href="/dashboard/exec">User Dashboard</a></li>
+                            <li><a href="/dashboard/exec" className="nav-item">User Dashboard</a></li>
                         }
                         {!loggedIn && 
-                            <li><a href="/register">Register</a></li>
+                            <li><a href="/register" className="nav-item">Register</a></li>
                         }
                         {loggedIn ? 
-                        <li><a href="/" onClick={this.onLogoutClick}>Log out</a></li>
+                        <li><a href="/" onClick={this.onLogoutClick} className="nav-item">Log out</a></li>
                         :
-                        <li><a href="/login">Log in</a></li>
+                        <li><a href="/login" className="nav-item">Log in</a></li>
                         }
                     </ul>
                 </div>
